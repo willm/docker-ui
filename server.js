fbd68875fdbab4d265525b81562652e1ee6826ed
Will Munn <william.munn@rubix.com>
2024-09-26 06:23:27 +0200

    chore: refactor split out delete and post handlers

--- server.js
+++ server.js
@@ -1,38 +1,16 @@
 import {createServer} from "http";
-import {
-  listContainers,
-  deleteContainer,
-  createContainer,
-  startContainer,
-} from "./lib/docker-client.js";
-import * as containersView from "./lib/containers-view.js";
 import {Router} from "./lib/router.js";
-import * as query from "node:querystring";
 import {listHandler} from "./lib/list-handler.js";
+import {postHandler} from "./lib/post-handler.js";
+import {deleteHandler} from "./lib/delete-handler.js";
 
-createServer(async (req, res) => {
+createServer((req, res) => {
   const router = Router(req, res);
   router.get(new RegExp("/$"), listHandler);
 
-  router.delete(
-    /\/containers\/(?<id>[0-9a-z]+)$/,
-    async ({matches, respond}) => {
-      const deleteRes = await deleteContainer({id: matches.groups.id});
-      const containerList = await listContainers();
-      respond(200, {}, containersView.render(containerList));
-    }
-  );
+  router.delete(/\/containers\/(?<id>[0-9a-z]+)$/, deleteHandler);
 
-  router.post(/\/containers$/, async ({matches, req, body, respond}) => {
-    const opts = query.parse(body);
-    const container = await createContainer({
-      ...opts,
-      cmd: opts.cmd.split(" "),
-    });
-    const res = await startContainer({id: container.Id});
-    const containerList = await listContainers();
-    respond(201, {}, containersView.render(containerList));
-  });
+  router.post(/\/containers$/, postHandler);
 
   router.route();
 }).listen(4000);
