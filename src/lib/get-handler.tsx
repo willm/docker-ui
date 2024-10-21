5d8fec51ed226863cc91197740ffaa336b961dfd
Will Munn <william.munn@rubix.com>
2024-10-21 08:22:09 +0200

    fix: container display bug multiplying containers

--- src/lib/get-handler.tsx
+++ src/lib/get-handler.tsx
@@ -1,8 +1,14 @@
 import type {Handler} from "hono";
 import {listContainers} from "./docker-client.js";
-import {ContainersList} from "./components/ContainersList.js";
+import {ContainerCard} from "./components/ContainerCard.js";
 
 export const getHandler: Handler = async (ctx) => {
   const containerList = await listContainers();
-  return ctx.html(<ContainersList containers={containerList} />);
+  return ctx.html(
+    <>
+      {containerList.map((container) => (
+        <ContainerCard container={container} />
+      ))}
+    </>
+  );
 };
